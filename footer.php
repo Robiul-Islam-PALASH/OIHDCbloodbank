459df2a66cd7cf6e28946fc1879157309f86249e
Robiul Islam Palash <89980602+Robiul-Islam-PALASH@users.noreply.github.com>
2021-09-02 11:11:07 -0700

    commit to master

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="footer text-center bg-light">
+	© Copyright <span id="demo"></span> <span class="brand">Orin Islami Hospital & Diagnostic Centre </span> All Rights Reserved by Robiul Islam PALASH
+	<script>
+		date = new Date();
+    	document.getElementById("demo").innerHTML=date.getFullYear();
+    </script>
+</footer>
---
+++ index.php
@@ -0,0 +1,164 @@
+<?php 
+    session_start();
+
+    ?>
+<!DOCTYPE html>
+<html>
+<?php $title="Bloodbank | home page"; ?>
+<?php require 'head.php'; ?>
+<body>
+    <?php require 'header.php'; ?>
+
+    <div class="container cont">
+
+   <?php require 'message.php'; ?>
+
+        
+                <div class="banner">
+                    <img src="image/banner.png" class="card-img-top">
+                </div>
+            </div>
+
+            <?php 
+            /*
+            <div class="col-lg-9">
+            <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">A+</div>
+                    <div class="card-body">
+                        If you are A+: You can give blood to A+ and AB+. You can receive blood from A+, A-, O+ and O-
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">A-</div>
+                    <div class="card-body">
+                        If you are A-: You can give blood to A-, A+, AB- and AB+. You can receive blood from A- and O-. 
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">B+</div>
+                    <div class="card-body">
+                         You can give blood to A+ and AB+. You can receive blood from A+, A-, O+ and O-.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">B-</div>
+                    <div class="card-body">
+                        If you are B-: You can give blood to B-, B+, AB+ and AB-, You can receive blood from B- and O-.You can give blood to B+ and AB+.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">AB+</div>
+                    <div class="card-body">
+                         People with AB+ blood can receive red blood cells from any blood type. This means that demand for AB+ can donate with AB only.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">AB-</div>
+                    <div class="card-body"> 
+                         AB- patients can receive red blood cells from all negative blood types.
+                         AB- can give red blood cells to both AB- and AB+ blood types.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">O+</div>
+                    <div class="card-body">
+                        Blood O+ can donate to A+, B+, AB+ and O+ Blood
+                        Group O can donate red blood cells to anybody. It's the universal donor.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">O-</div>
+                    <div class="card-body">
+                        O- can donate to A+, A-, B+, B-, AB+, AB-, O+ and O- Blood
+                        People with O negative blood can only receive red cell donations from O negative donors.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">PALASH</div>
+                    <div class="card-body">
+                        <i class="fa fa-address-book"> </i> <a >Bloodbank Admin</a><br>
+                        <i class="fa fa-envelope"> </i> <a > oihdc.blood@gmail.com</a><br>
+                        <i class="fa fa-mobile"> </i> <a > +8801324440830(admin)</a><br>
+                        <i class="fa fa-mobile"> </i> <a > +8801794830011(Office)</a><br><br>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+
+        </div>
+        */
+        ?>
+
+        <div class="row">
+            <div class="col-lg-6 rounded mb-5">
+
+            </div>
+            <div class="col-lg-6 rounded mb-5">
+                 </div>
+        </div>
+
+        <div class="row mb-5">
+            <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">OIHDC BLOODBANK</div>
+                <div class="card-body">
+                    <dt>About OIHDC(ORIN ISLAMI HOSPITAL & DIAGNOSTIC CENTRE) Bloodbank</dt>
+                    <dd>OIHDC Blood Bank is a place for testing, screening, collecting, processing and storing of whole blood, packed red blood cells, fresh frozen plasma and platelet concentrate gathered as a result of blood donation by family members, friends and volunteer donors, preserved for patients use in this hospital and also other hospitals on request. We are committed to ensure 24hrs service of providing most secure blood transfusion policy and maintain quality in every aspects. Orin Islami Hospital & Diagnostic Centre blood bank provides safest possible blood for the patients which follows the Bangladesh Association of Blood Bank (BABB) guideline, USFDA guideline is used for donor infectious diseases screening (HBs Ag, HIV-1 &II, Anti – HCV, VDRL, Malaria).</dd>
+                    <dt> Blood Donation Ability</dt>
+                    <dd>Blood donation is a healthy habit that helps blood renewal and it is not hazardous at all. The volume of blood collected from a donor is about 450ml, this volume is compensated within 24hrs of donation and body replace the lost red cells within several weeks. Eligible healthy donors (age 18 to 60 years, having Hb >12gm/dl and weight >45) male can donate blood every 4 months interval and female every 6 months interval. Platelet Apheresis donor’s age should be 18 to 60 years weight >50kg and has not taken the medicine Plavix/Ticlid for last 14 days.</dd>
+                    <dt>INSTRUCTIONS PRIOR BLOOD DONATION</dt>
+                    <dd><ul><li>Do not donate blood on an empty stomach.</li>
+                        <li>Maintain a gap of 30 min after meal.</li>
+                        <li>Good sleep on the previous night.</li>
+                        <li>Drink water (2 glass) before blood donation</li>
+                        <li>Do not donate blood if you have fever, taking antibiotics or aspirin or antihistamine or insulin.</li>
+                        <li>Do not take alcohol 24hrs prior blood donation.</li>
+                        <li>Do not smoke 30 min before or after blood donation.</li>
+                        <li>Avoid driving, vigorous exercise or playing tennis for at least 2-3hrs after blood donation.</ul>
+                    </ul>
+
+</dd>
+                    <dt>NSTRUCTIONS AFTER BLOOD DONATION</dt>
+                    <dd>
+                        <ul><li>Eat and Drink something before leaving the blood donor area.</li>
+                            <li>Drink more fluid for next 4 hours.</li>
+                            <li>If there is bleeding from phlebotomy site raise arm and apply pressure to the site.</li>
+                            <li>If fainting or dizziness occur either lie down or sit with the head between the knees.</li>
+                            <li>If the symptom persist either telephone or returns to the blood bank or see a doctor.</li>
+                            <li>Resume all normal activity if asymptomatic.</li>
+                            <li>Remove bandage after 1 hour.</li>
+                            <li>Take some rest if you need.</li>
+                        </ul>
+                    </dd>
+                    
+                    Visit here to get more health tips.
+                    <a href="https://www.who.int/philippines/news/feature-stories/detail/20-health-tips-for-2020" target="_blank"> World Health Organisation</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require 'footer.php'; ?>
+
+</body>
+</html>
